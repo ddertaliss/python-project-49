6989ed14a92b427c7689d665bf169fe51bb20b83
ddertaliss <100406143+fawarris@users.noreply.github.com>
2023-08-14 21:57:52 +1000

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,15 @@
 from random import randint
-from brain_games.cli import welcome_name
 def main():
-    def even():
-            print('Welcome to the Brain Games!') 
-            print('Answer "yes" if the number is even, otherwise answer "no".')
+        print('Welcome to the Brain Games!')
+        name = input('May I have your name? ')
+        print(f'Hello, {name}')
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+
+        count = 0
+        for _ in range(3):
             num = randint(1, 30)
             print(f'Question: {num}')
             answer = input('Your answer:')
-    
             correct_answer = ''
             if num % 2 == 0:
                 correct_answer = 'yes'
@@ -15,17 +17,10 @@ def main():
                 correct_answer = 'no'
             if answer == correct_answer:
                 print('Correct!')
-                return True
-            else:
-                print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}. Let's try again, {welcome_name}!")
-                return False
-    count = 0
-    while count != 3:
-        if even() == True:
-            count += 1
-        else:
-            count = 0
-            continue
-    if count == 3:
-        print(f'Congratulations, {welcome_name}!')
+                count += 1
+                if count != 3:
+                     continue
+                else:
+                     print(f"Congratulations, {name}!")
+                     break
 
