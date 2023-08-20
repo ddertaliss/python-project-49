2374eb185e7f83ae7cf9768109502b31cb1173a4
hjdfgjf <khkjsdfh>
2023-08-20 20:15:26 +1000

    added greeting and name

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
-from brain_games.cli import welcome_user
+from brain_games.scripts.cli import welcome_user
+def greet():
+    print('Welcome to the Brain Games!')
 def main():
-	print('Welcome to the Brain Games!')
-if __name__ == '__main__':
-	main()
+    greet()
+    welcome_user()
 
-welcome_user()
+if __name__ == '__main__':
+    main()
+
